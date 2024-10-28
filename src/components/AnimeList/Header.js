ff2bd3c8d07a2b03450469ca3418cffe79907f5c
midas79 <dionisius.suryajaya@gmail.com>
2024-10-28 23:58:27 +0700

    Adding Not Found Page, Loading, and Theming UI
    bingung color pallete UI

--- src/components/AnimeList/Header.js
+++ src/components/AnimeList/Header.js
@@ -3,11 +3,11 @@ import Link from "next/link";
 const Header = ({ title, linkHref, linkTitle }) => {
   return (
     <div className="flex justify-between items-center p-4">
-      <h1 className="text-2xl font-bold">{title}</h1>
+      <h1 className="text-2xl font-bold text-primary">{title}</h1>
       {linkHref && linkTitle ? (
         <Link
           href={linkHref}
-          className="md:text-xl text:sm underline hover:text-indigo-500 transition-all"
+          className="md:text-xl text:sm underline hover:text-accent text-primary transition-all"
         >
           {linkTitle}
         </Link>

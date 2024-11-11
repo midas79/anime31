643e6ef6d64590b4273fb9744c3cf5984b4672cf
Joseph Felix <92004207+Extra20Percent@users.noreply.github.com>
2024-11-11 23:57:31 +0700

    update

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -1,18 +1,27 @@
-import NextAuth from "next-auth"
-import githubAuth from "next-auth/providers/github"
+import NextAuth from "next-auth";
+import githubAuth from "next-auth/providers/github";
 
+// Menentukan clientId dan clientSecret sesuai environment
+const clientId =
+  process.env.NODE_ENV === "production"
+    ? process.env.GITHUB_CLIENT_Jo
+    : process.env.GITHUB_CLIENT_Diyon;
 
+const clientSecret =
+  process.env.NODE_ENV === "production"
+    ? process.env.GITHUB_SECRET_Jo
+    : process.env.GITHUB_SECRET_Diyon;
 
 const authOption = {
-    providers: [
-        githubAuth ({
-            clientId: process.env.GITHUB_CLIENT,
-            clientSecret: process.env.GITHUB_SECRET
-        })
-    ],
-    secret: process.env.NEXTAUTH_SECRET
-}
-   
-const handler = NextAuth(authOption)
+  providers: [
+    githubAuth({
+      clientId: clientId,
+      clientSecret: clientSecret,
+    }),
+  ],
+  secret: process.env.NEXTAUTH_SECRET,
+};
 
-export {handler as GET, handler as POST}
+const handler = NextAuth(authOption);
+
+export { handler as GET, handler as POST };
